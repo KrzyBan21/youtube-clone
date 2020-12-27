ab32833bd078193bb9e8c05eb458b3e6312e829d
KrzyBan21 <33942315+KrzyBan21@users.noreply.github.com>
2020-12-27 18:43:19 +0100

    Refactoring filmList reducer

--- src/store/reducers/filmList.js
+++ src/store/reducers/filmList.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject, updateArray } from "../utils";
 
 const initialState = {
   isLoading: false,
@@ -10,57 +11,71 @@ const initialState = {
   nextPageToken: "",
 };
 
+const getFilmsStart = (state, action) => {
+  if (action.isMoreFilms) {
+    const objectToUpdate = { isLoadingMore: true, error: null };
+    return updateObject(state, objectToUpdate);
+  } else {
+    const objectToUpdate = {
+      isLoading: true,
+      error: null,
+      nextPageToken: action.pageToken,
+    };
+    return updateObject(state, objectToUpdate);
+  }
+};
+
+const getFilmsSuccess = (state, action) => {
+  if (action.isMoreFilms) {
+    const filmsArray = updateArray(state.films, action.films);
+    const objectToUpdate = {
+      isLoadingMore: false,
+      films: filmsArray,
+      nextPageToken: action.pageToken,
+    };
+    return updateObject(state, objectToUpdate);
+  } else {
+    const objectToUpdate = {
+      isLoading: false,
+      films: action.films,
+      nextPageToken: action.pageToken,
+    };
+    return updateObject(state, objectToUpdate);
+  }
+};
+
+const getFilmsFail = (state, action) => {
+  const objectToReturn = {
+    isLoading: false,
+    isLoadingMore: false,
+    error: action.error,
+  };
+
+  return updateObject(state, objectToReturn);
+};
+
+const selectFilm = (state, action) => {
+  const objectToReturn = { video: action.video };
+  return updateObject(state, objectToReturn);
+};
+
+const setSearchedText = (state, action) => {
+  const objectToUpdate = { text: action.text };
+  return updateObject(state, objectToUpdate);
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.GET_FILMS_START:
-      if (action.isMoreFilms) {
-        return {
-          ...state,
-          isLoadingMore: true,
-          error: null,
-        };
-      } else {
-        return {
-          ...state,
-          isLoading: true,
-          error: null,
-          nextPageToken: action.pageToken,
-        };
-      }
-
+      return getFilmsStart(state, action);
     case actionTypes.GET_FILMS_SUCCESS:
-      if (action.isMoreFilms) {
-        return {
-          ...state,
-          isLoadingMore: false,
-          films: [...state.films, ...action.films],
-          nextPageToken: action.pageToken,
-        };
-      } else {
-        return {
-          ...state,
-          isLoading: false,
-          films: action.films,
-          nextPageToken: action.pageToken,
-        };
-      }
-
+      return getFilmsSuccess(state, action);
     case actionTypes.GET_FILMS_FAIL:
-      return {
-        ...state,
-        isLoading: false,
-        error: action.error,
-      };
+      return getFilmsFail(state, action);
     case actionTypes.SELECT_FILM:
-      return {
-        ...state,
-        video: action.video,
-      };
+      return selectFilm(state, action);
     case actionTypes.SET_SEARCHED_TEXT:
-      return {
-        ...state,
-        text: action.text,
-      };
+      return setSearchedText(state, action);
     default:
       return state;
   }
